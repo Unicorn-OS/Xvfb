3d87be381c6766108cc70ce6eb8e15e44f99561d
DarkStar☀ <61005029+Twin-Flame@users.noreply.github.com>
2023-12-11 18:00:52 -0600

    Create works.sh

---
+++ src/example/in:/Distrobox/works.sh
@@ -0,0 +1,14 @@
+inDistrobox(){
+  distrobox create --pull --image ubuntu --name test --home ~/distrobox/test
+}
+
+Server(){
+  # https://jasonmurray.org/posts/2021/x11vnc/
+  sudo apt-get install x11vnc xvfb fluxbox
+  sudo x11vnc -create -env FD_PROG=/usr/bin/fluxbox -env X11VNC_CREATE_GEOM=${1:-1024x768x16} -nopw -listen 127.0.0.1 -forever
+}
+
+Client(){
+  sudo apt install tigervnc-viewer
+  vncviewer 127.0.0.1
+}
