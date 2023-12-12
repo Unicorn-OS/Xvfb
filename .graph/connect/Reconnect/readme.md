48fffb312545d2376d54e6f5dd1a053c752f0d66
🦄Pegasus (DarkStar☀) <darkstar@unisocial.net>
2023-12-12 01:27:35 -0600

    in:

---
+++ .graph/connect/Reconnect/readme.md
@@ -0,0 +1,5 @@
+# Cannot Reconnect
+## Solution:
+`x11vnc -reopen`
+
+Discuss: https://superuser.com/questions/1731619/cannot-reconnect-to-x11vnc
--- src/example/in:/Distrobox/works.sh
+++ src/example/in:/Distrobox/works.sh
@@ -9,7 +9,7 @@ inDistrobox(){
 Server(){
   # https://jasonmurray.org/posts/2021/x11vnc/
   sudo apt install -y x11vnc xvfb fluxbox
-  sudo x11vnc -create -env FD_PROG=/usr/bin/fluxbox -env X11VNC_CREATE_GEOM=${1:-1024x768x16} -nopw -listen 127.0.0.1 -forever
+  sudo x11vnc -create -env FD_PROG=/usr/bin/fluxbox -env X11VNC_CREATE_GEOM=${1:-1024x768x16} -nopw -listen 127.0.0.1 -reopen -forever
 }
 
 Client(){
